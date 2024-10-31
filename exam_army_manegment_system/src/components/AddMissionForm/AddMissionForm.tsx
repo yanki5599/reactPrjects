07bba2e435b7688d586f2cefbc3615d384552b1c
yakov gottlib <yanki5599@gmail.com>
2024-10-31 10:36:59 +0200

    done addMissionForm

--- exam_army_manegment_system/src/components/AddMissionForm/AddMissionForm.tsx
+++ exam_army_manegment_system/src/components/AddMissionForm/AddMissionForm.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import './AddMissionForm.css';
+import React, { FormEvent, useState } from "react";
+import "./AddMissionForm.css";
+import { Mission, MissionStatus, Priority } from "../../types/mission";
+interface AddMissionFormProps {
+  handleSubmitFunc: (mission: Mission) => void;
+}
 
-interface AddMissionFormProps {}
+const AddMissionForm: React.FC<AddMissionFormProps> = ({
+  handleSubmitFunc,
+}) => {
+  const [formData, setFormData] = useState<Mission>({
+    name: "",
+    status: MissionStatus.Pending,
+    priority: Priority.Low,
+    description: "",
+  });
 
-const AddMissionForm:React.FC<AddMissionFormProps> = ({}) => {
-    return (
-        <div className="AddMissionForm">
-            <h1>AddMissionForm Component</h1>
-        </div>
-    );
+  function handleChange(e: any) {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function localHandleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!formData.name.trim() || !formData.description.trim()) return;
+    handleSubmitFunc(formData);
+  }
+
+  return (
+    <form className="AddMissionForm" onSubmit={localHandleSubmit}>
+      <input
+        type="text"
+        name="name"
+        required
+        placeholder="Enter name:"
+        onChange={handleChange}
+      />
+      <select name="missionStatus" required onChange={handleChange}>
+        {Object.keys(MissionStatus).map((key, idx) => (
+          <option key={"statusOpt" + idx} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+      <select name="missionStatus" required onChange={handleChange}>
+        {Object.keys(Priority).map((key, idx) => (
+          <option key={"priorityOpt" + idx} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+      <textarea
+        name="description"
+        required
+        placeholder="description:"
+        onChange={handleChange}
+      />
+      <button type="submit">Add Mission</button>
+    </form>
+  );
 };
 
 export default AddMissionForm;
