ce0a19c12e8b2a502f7fa431b03103e77c153648
yakov gottlib <yanki5599@gmail.com>
2024-11-07 09:50:07 +0200

    added floors slice;

--- NimrodiBuilding/src/store/features/floorreducer/floorreducerSlice.ts
+++ NimrodiBuilding/src/store/features/floorreducer/floorreducerSlice.ts
@@ -1,15 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import buildings from "../../../data/building.json";
 
-interface FloorreducerStateType {}
-const initialState: FloorreducerStateType = {};
+interface FloorreducerStateType {
+  floors: { [floorName: string]: boolean };
+}
+const initialState: FloorreducerStateType = {
+  floors: buildings.reduce(
+    (obj, item) => Object.assign(obj, { [item.name]: false }),
+    {}
+  ),
+};
 
 export const FloorreducerSlice = createSlice({
   initialState,
   name: "floorreducer",
   reducers: {
-    changeAccess: (state, action: PayloadAction<{}>) => {},
+    changeAccess: (state, action: PayloadAction<{ floorName: string }>) => {
+      state.floors[action.payload.floorName] =
+        !state.floors[action.payload.floorName];
+    },
   },
 });
 
-export const { addFloorreducer } = FloorreducerSlice.actions;
+export const { changeAccess } = FloorreducerSlice.actions;
 export default FloorreducerSlice.reducer;
