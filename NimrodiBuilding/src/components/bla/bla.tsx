97154161ca4c2eb9eb799cd69cee4a20a05e3f35
yakov gottlib <yanki5599@gmail.com>
2024-11-13 15:21:12 +0200

    delete bla

---
+++ NimrodiBuilding/src/components/bla/bla.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './bla.css';
+
+interface blaProps {}
+
+const bla:React.FC<blaProps> = ({}) => {
+    return (
+        <div className="bla">
+            <h1>bla Component</h1>
+        </div>
+    );
+};
+
+export default bla;
